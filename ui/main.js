9aef1e11b2f6d3b06d1b39e8095c7629a29962a3
Aravind parihar <arvindpariharmali@gmail.com>
2017-08-12 12:17:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var img=document.getElementById('modi');
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft+5;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick= function () {
+    var interval=setInterval(moveRight,50);
+};
